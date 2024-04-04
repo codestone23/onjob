6ecba14cda4905e1936f1e0b4c00a068d7eb0029
Đạt Phạm Gia <phamdat04052003@gmail.com>
2024-04-04 08:31:03 +0700

    first-commit

---
+++ day1/number.js
@@ -0,0 +1,111 @@
+// homework number
+// bai 1 - tinh the tich hinh cau
+
+let sphericalVolume = (radius) => {
+    if(radius > 0){
+        return Math.round((4 / 3) * Math.PI * Math.pow(radius,3) * 100) / 100;
+    }
+    console.log("ban kinh khong hop le!")
+}
+
+let radius = 4;
+console.log(sphericalVolume(radius));
+
+// bai 2 - Tinh tong tat ca cac so nguyen nam giua chung
+
+let sumNumber = (firstNumber,secondNumber) => {
+    if(firstNumber >= secondNumber - 1){
+        return 0;
+    }
+    let count = 0;
+    for(let i = firstNumber + 1; i < secondNumber; i++){
+        count += i;
+    }
+    return count;
+}
+
+let firstNumber = 3, secondNumber = 8;
+console.log(sumNumber(firstNumber, secondNumber));
+
+// bai 3 - kiem tra so nguyen to
+
+let isPrime = (number) => {
+    if (number < 2) {
+        return false;
+    }
+    
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+
+let number1 = 4;
+let number2 = 101;
+if(isPrime(number1)){
+    console.log("number1 is prime");
+}else{
+    console.log("number1 is not prime");
+}
+if(isPrime(number2)){
+    console.log("number2 is prime");
+}else{
+    console.log("number2 is not prime");
+}
+
+// bai 4 - tong cac so nguyen to nho hon n
+
+let n = 3;
+let cnt = 0;
+if(n >= 2){
+    cnt = 2;
+    for(let i = 3; i <= n; i++){
+        if(isPrime(i)){
+            cnt += i;
+        }
+    }
+    console.log(`tong cac so nguyen to nho hơn ${n} là ${cnt}`);
+}else{
+    console.log(`tong cac so nguyen to nho hơn ${n} là 0`);
+}
+
+// bai 5 - viet ham tinh tong tat ca cac uoc cua so do
+
+let total = 0;
+
+let totalDivisor = (num) =>{
+    for(let i=1; i<=num/2; i++){
+        let rel = num%i;
+        if(num%i ==0){
+            total += i;
+        }
+        if(num%rel == 0){
+            total += rel;
+        }
+        if(i == rel){
+            total -= i;
+        }
+    }
+    return total;
+}
+
+let num = 30;
+console.log(`tong uoc so cua ${num} bang ${totalDivisor(num)}`);
+
+
+// bai 6 - viết hàm sắp xếp lại các chữ số 
+
+let sortNumber = (number6) => {
+    let arr = number6.toString().split("");
+    arr.sort();
+    let result = parseInt(arr.join(""));
+    return result;
+}
+
+let number6 = 145500123;
+console.log(`chu so nho nhat la ${sortNumber(number6)}`);
+
---
+++ day1/string.js
@@ -0,0 +1,89 @@
+// homework string
+// bai 1 - kiem tra 1 chuoi co nam trong chuoi con lai hay khong
+
+let checkStringExist = (str1,str2) => {
+    return str1.includes(str2);
+}
+
+let str1 = "Amela A Beta";
+let str2 = "Beta";
+console.log(checkStringExist(str1,str2));
+
+// bai 2 - viet ham rut ngan chuoi
+
+let shortenString = (str) => {
+    let relString = str.substring(0,8) + '...';
+    return relString;
+}
+
+let str = "Xin chao cac ban";
+console.log(`Chuoi thay the la: ${shortenString(str)}`)
+
+// bai 3 - viet ham sao chep chuoi do len 10 lan
+
+let repeatStr = (str) => {
+    return str.repeat(10);
+}
+
+let str3 = "ABC";
+console.log(repeatStr(str3));
+
+// bai 4 - viet ham sao chep chuoi len 10 lan cach nhau boi dau gach ngang
+
+
+let repeatString = (str) => {
+    let arr = [];
+    for(let i=0; i< 10; i++){
+        arr.push(str);
+    }
+    return arr.join('-');
+
+    // return Array(10).fill(str).join("-");
+}
+
+let str4 = "ABC";
+console.log(repeatString(str4));
+
+// bai 5 - viet ham sao chep chuoi len n lan cach nhau boi dau gach ngang
+
+let repeatStringN = (str,number) => {
+    let arr = [];
+    for(let i=0; i< number; i++){
+        arr.push(str);
+    }
+    return arr.join('-');
+
+    // return Array(number).fill(str).join("-");
+}
+
+let str5 = "ABC";
+let n = 6
+console.log(repeatStringN(str5,n));
+
+// bai 6 - viet ham dao nguoc chuoi
+
+let reverseString = (str) =>{
+    return str.split("").reverse().join("");
+}
+
+let str6 = "hello world";
+console.log(`dao nguoc chuoi cua str6 la: ${reverseString(str6)}`);
+
+// bai 7 - viet ham check doi xung
+
+let checkSymmetry = (str) => {
+    let replaceStr = str.replace(/\s+/g, "").split("").reverse().join("");
+    return replaceStr === str.replace(/\s+/g, "");
+}
+
+let str7 = "Race car";
+console.log(checkSymmetry(str7.toLowerCase()));
+
+// bai 8 - viet ham kiem tra xem chuoi co viet hoa hay khong 
+
+let checkUpperCase = (str) => {
+    return str === str.toUpperCase();
+}
+
+let str8 = "Hello World";
+console.log(checkUpperCase(str8));
